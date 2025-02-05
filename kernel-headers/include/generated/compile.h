6af694f7ebd681831c37b5dd085c3f99ba46551d
GitFASTBOOT <ahmedshahinx0x@gmail.com>
2025-02-05 20:53:04 +0200

    penangf: import kernel-headers

---
+++ kernel-headers/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#2 SMP PREEMPT Wed Feb 5 15:08:33 UTC 2025"
+#define LINUX_COMPILE_BY "devsrv"
+#define LINUX_COMPILE_HOST "bwa-xsrv"
+#define LINUX_COMPILER "Android (7284624, based on r416183b) clang version 12.0.5 (https://android.googlesource.com/toolchain/llvm-project c935d99d7cf2016289302412d708641d52d2f7ee), LLD 12.0.5 (/buildbot/src/android/llvm-toolchain/out/llvm-project/lld c935d99d7cf2016289302412d708641d52d2f7ee)"
